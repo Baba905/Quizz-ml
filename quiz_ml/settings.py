41c0c6290f97da3085adc138d82788f94c2c6243
Baba905 <baba78122@gmail.com>
2022-08-10 02:19:52 +0200

    change the AUTH_USER_MODEL value in setings file

--- quiz_ml/settings.py
+++ quiz_ml/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # for heroku ***************
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = 'static'
-AUTH_USER_MODEL = 'api_fewnu_compta.User'
+#AUTH_USER_MODEL = 'api_fewnu_compta.User'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') #le chemin du serveur pour stocker les fichiers sur l’ordinateur. 
 MEDIA_URL = '/media/'# comment l’URL de référence permettant au navigateur d’accéder aux fichiers via Http.
